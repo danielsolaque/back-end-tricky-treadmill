6e9f164eb625f1c7d4c364584e0948e603390019
Daniel Solaque <danielsolaque@pursuit.org>
2024-01-12 08:22:16 -0500

    first commit with code track

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get(('/'), (req, res) => {
-    res.send('This is the API for trickyTreadmill.com ')
+    res.send('This is the API for trickyTreadmill.com! ')
 })
 
 module.exports = app;
