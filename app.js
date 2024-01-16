0e304d0b3422d41f67f61f2737b7fcdf6aca0361
Daniel Solaque <danielsolaque@pursuit.org>
2024-01-15 20:37:12 -0500

    implementing all brands and top brands routes

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
+const data = require("./data");
 
 const app = express();
 
-app.get(('/'), (req, res) => {
-    res.send('This is the API for trickyTreadmill.com! ')
-})
+//Routes:
+
+//Whole list of machines (all brands) when click to botton  =>> /all-brands
+//Partial List of machines (top brands)=>> /top-brands
+
+//==>>lis of models ==>/all-models/:brand
+//==>>lis of reviews per model==>/all-reviews/:model
+//Partial list of reviews -top 3 most useful- => /top-reviews
+//Botton create a new review==>> /review
+
+app.get("/", (req, res) => {
+  res.send("This is the API for trickyTreadmill.com! ");
+});
+
+app.get("/all-brands", (req, res) => {
+  res.json(data.brands);
+});
+
+app.get("/top-brands", (req, res) => {
+  const topBrandsFilter = data.brands.filter((brand) => brand.isTop);
+  res.json(topBrandsFilter);
+});
 
 module.exports = app;
---
+++ data.js
@@ -0,0 +1,65 @@
+/**
+ * Brand model
+ * {
+ *      id: string,
+ *      title: string,
+ *      isTop: boolean,
+ * }
+ */
+
+const brands = [
+  {
+    id: "CYBEX",
+    title: "CYBEX",
+    isTop: true,
+  },
+  {
+    id: "Hammer Strength",
+    title: "Hammer Strength",
+    isTop: true,
+  },
+  {
+    id: "Life Fitness",
+    title: "Life Fitness",
+    isTop: true,
+  },
+  {
+    id: "Matrix",
+    title: "Matrix",
+    isTop: true,
+  },
+  {
+    id: "Nautilus",
+    title: "Nautilus",
+    isTop: true,
+  },
+  {
+    id: "Precor",
+    title: "Precor",
+    isTop: true,
+  },
+  {
+    id: "StarTrac",
+    title: "StarTrac",
+    isTop: true,
+  },
+  {
+    id: "BH",
+    title: "BH",
+    isTop: false,
+  },
+  {
+    id: "NordicTrack",
+    title: "NordicTrack",
+    isTop: false,
+  },
+  {
+    id: "Peloton",
+    title: "Peloton",
+    isTop: false,
+  },
+];
+
+module.exports = {
+  brands,
+};
