4c1468d13f8010372a1041f38a0b13a9edb86467
Daniel Solaque <danielsolaque@pursuit.org>
2024-01-17 23:26:16 -0500

    Implementing routes of models and reviews given brand and model respectively + local data base creation

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 
 //Routes:
 
-//Whole list of machines (all brands) when click to botton  =>> /all-brands
-//Partial List of machines (top brands)=>> /top-brands
+// - [x] Whole list of machines (all brands) when click to botton  =>> /all-brands
+// - [x] Partial List of machines (top brands)=>> /top-brands
+// - [x] Whole list of models associated to a specific brand => /models/:brandId (/models/8)
+// - [x] Whole list of reviews associated to a specific model => /reviews/:modelId
+// - [x] Whole list of reviews associated to a specific category => /reviews/:categoryId
 
-//==>>lis of models ==>/all-models/:brand
+
 //==>>lis of reviews per model==>/all-reviews/:model
 //Partial list of reviews -top 3 most useful- => /top-reviews
 //Botton create a new review==>> /review
@@ -18,12 +21,34 @@ app.get("/", (req, res) => {
 });
 
 app.get("/all-brands", (req, res) => {
-  res.json(data.brands);
+    res.json(data.brands)
 });
 
 app.get("/top-brands", (req, res) => {
-  const topBrandsFilter = data.brands.filter((brand) => brand.isTop);
-  res.json(topBrandsFilter);
+    const filteredBrands = data.brands.filter((brand) => brand.isTop);
+
+    res.json(filteredBrands);
 });
+
+//  { 
+//   brand_id: 7, // number 
+//   model_id: 3,
+//   title: "Startrac 800",
+// }
+// 7
+app.get("/models/:brandId", (req, res) => {
+  const brandId = req.params.brandId; // string
+  const brandIdToNumber = Number(brandId);
+  const filteredModels = data.models.filter((model) => model.brand_id === brandIdToNumber);
+
+  res.json(filteredModels);
+
+})
+
+app.get("/reviews/:modelId", (req, res) => {
+  const modelId = Number(req.params.modelId);
+  const filteredReviews = data.reviews.filter((review) => review.model_id === modelId);
+  res.json(filteredReviews);
+})
 
 module.exports = app;
--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 /**
- * Brand model
+ * Brand schema
+ *
  * {
- *      id: string,
+ *      brand_id: number,
  *      title: string,
  *      isTop: boolean,
  * }
@@ -9,57 +10,203 @@
 
 const brands = [
   {
-    id: "CYBEX",
+    brand_id: 1,
     title: "CYBEX",
     isTop: true,
   },
   {
-    id: "Hammer Strength",
+    brand_id: 2,
     title: "Hammer Strength",
     isTop: true,
   },
   {
-    id: "Life Fitness",
+    brand_id: 3,
     title: "Life Fitness",
     isTop: true,
   },
   {
-    id: "Matrix",
+    brand_id: 4,
     title: "Matrix",
     isTop: true,
   },
   {
-    id: "Nautilus",
+    brand_id: 5,
     title: "Nautilus",
     isTop: true,
   },
   {
-    id: "Precor",
+    brand_id: 6,
     title: "Precor",
     isTop: true,
   },
   {
-    id: "StarTrac",
+    brand_id: 7,
     title: "StarTrac",
     isTop: true,
   },
   {
-    id: "BH",
+    brand_id: 8,
     title: "BH",
     isTop: false,
   },
   {
-    id: "NordicTrack",
+    brand_id: 9,
     title: "NordicTrack",
     isTop: false,
   },
   {
-    id: "Peloton",
+    brand_id: 10,
     title: "Peloton",
     isTop: false,
   },
 ];
 
+
+const models = [
+  {
+    brand_id: 10,
+    model_id: 1,
+    title: "Bike+",
+  },
+  {
+    brand_id: 10,
+    model_id: 2,
+    title: "Bike Basic",
+  },
+  { 
+    brand_id: 7,
+    model_id: 3,
+    title: "Startrac 800",
+  },
+  {
+    brand_id: 7,
+    model_id: 4,
+    title: "Startrac 3000",
+  },
+  {
+    brand_id: 1,
+    model_id: 5,
+    title: "Cybex 700A",
+  },
+  { 
+    brand_id: 1,
+    model_id: 6,
+    title: "Cybex 800A",
+  },
+  { 
+    brand_id: 1,
+    model_id: 7,
+    title: "Cybex 800A",
+  }
+];
+
+const reviews = [
+  {
+    review_id: 1,
+    problem_title: "Louder noises",
+    name: "Jordan",
+    category_id: 3,
+    model_id: 2,
+    brand_id: 10,
+    problem_description: "Treadmill is making noises once I start running",
+    problem_solution:
+      "Normally this is due to a worn drive belt, is highly recommended to change it",
+  },
+  {
+    review_id: 2,
+    problem_title: "Display malfunction",
+    name: "Emily",
+    category_id: 2,
+    model_id: 5,
+    brand_id: 7,
+    problem_description:
+      "The display screen is not showing accurate information",
+    problem_solution:
+      "Try resetting the treadmill and if the issue persists, contact customer support.",
+  },
+  {
+    review_id: 3,
+    problem_title: "Uneven belt movement",
+    name: "Alex",
+    category_id: 4,
+    model_id: 1,
+    brand_id: 12,
+    problem_description: "The treadmill belt is moving unevenly during use",
+    problem_solution:
+      "Check if the treadmill belt is properly aligned and adjust it if necessary. Also, ensure proper lubrication.",
+  },
+  {
+    review_id: 4,
+    problem_title: "Resistance issues",
+    name: "Chris",
+    category_id: 1,
+    model_id: 3,
+    brand_id: 8,
+    problem_description:
+      "The resistance on the elliptical machine is not working properly",
+    problem_solution:
+      "Inspect the resistance mechanism and cables. Lubricate if necessary or seek professional assistance.",
+  },
+  {
+    review_id: 5,
+    problem_title: "Handlebar instability",
+    name: "Olivia",
+    category_id: 5,
+    model_id: 4,
+    brand_id: 9,
+    problem_description:
+      "The handlebars on the exercise bike feel unstable during use",
+    problem_solution:
+      "Tighten all screws and bolts on the handlebars. If the issue persists, check for any damage or wear.",
+  },
+  {
+    review_id: 6,
+    problem_title: "Console not powering on",
+    name: "Dylan",
+    category_id: 2,
+    model_id: 6,
+    brand_id: 11,
+    problem_description: "The console of the stationary bike is not turning on",
+    problem_solution:
+      "Check the power source and cables. If the problem persists, contact the manufacturer for technical support.",
+  },
+  {
+    review_id: 7,
+    problem_title: "Pedal misalignment",
+    name: "Sophia",
+    category_id: 5,
+    model_id: 2,
+    brand_id: 10,
+    problem_description: "The pedals on the elliptical machine are misaligned",
+    problem_solution:
+      "Adjust the pedal alignment and check for any loose connections. Tighten as needed for a smooth operation.",
+  },
+  {
+    review_id: 8,
+    problem_title: "Inaccurate heart rate readings",
+    name: "Ethan",
+    category_id: 1,
+    model_id: 4,
+    brand_id: 9,
+    problem_description:
+      "The heart rate monitor on the fitness tracker is providing inaccurate readings",
+    problem_solution:
+      "Ensure proper placement of the tracker and clean the sensors. If issues persist, contact customer support.",
+  },
+];
+
+
+const categories = [
+  {
+    category_id: 1,
+    category_name: "noises",
+    review_id: 1,
+  },
+];
+
 module.exports = {
   brands,
+  models,
+  reviews,
+  categories,
 };
